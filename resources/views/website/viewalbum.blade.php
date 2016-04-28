d0c1a77947b1d7ea63c3c9dc264f135728e2dc3e
JananiDivya <janani.ide@gmail.com>
2016-04-28 12:25:50 +0530

    album changes

--- resources/views/website/viewalbum.blade.php
+++ resources/views/website/viewalbum.blade.php
@@ -15,71 +15,39 @@
 	<main>
 		<section id="content">
 			<div class="container">
-				<div class="album-banner col-md-9" >
-					<div class="row" style="padding:25px;">
-						<div class="">
-							<h3>Elau Akkual - Download from MadShop</h3>
-							<p>Madmuzik records - Compiled by Arcane</p>
+				<div class="row artist-info">
+					<div class="col-sm-4 col-md-3">
+						@foreach($albumDetails as $album)
+						<div class="latest-content">
+							<div class="latest-content-image">
+								<img src="{{$album->album_art}}" alt="" class="img-responsive"/>
+							</div>
+							<div class="latest-content-info">
+								<div class="meta">
+									<div class="icon">
+										<i class="fa fa-user"></i>
+									</div>
+									<h4>{{$album->album_name}}</h4>
+									<!-- <p>{{$album->artist_title}}</p> -->
+								</div>
+							</div>
 						</div>
-						<div class="">
-							<div id="tagline" style="padding:0 0;"><h4>INFORMATION :</h4></div>
-							<p>In the language of the wise and ancient Maya, ‘Elau’ means ‘Color’ and ‘Akkual’ means Night; as a dedication to the new age of consciousness, we welcome 2012 with this collection of eclectic sounds from the night. A time for meditation, introspection,  and heightened awareness.</p>
-							<p>This compilation of hypnotic sounds will appeal to the deepest realms of your inner consciousness; these frequencies will help you meditate, heal and provide a positive platform for your deep introspection journey into the night till enlightenment.</p>
-							<p>We hope you enjoy listening to these sounds as much as we have had in getting them together, and of course much respect to the artists that work hard to retrieve them from the universe and bring these to us in little tidy packages.</p>
-							<p>Here is a preview of the tracks which will be available on a CD Release through Arabesque Distribution and for digital download: </p>
-						</div>
+						<ul class="share clearfix">
+							<li><a href="{{$album->album_youtube}}"><i class="fa fa-lg fa-facebook"></i></a></li>
+							<li><a href="{{$album->album_youtube}}"><i class="fa fa-lg fa-twitter"></i></a></li>
+							<li><a href="{{$album->album_youtube}}"><i class="fa fa-lg fa-flickr"></i></a></li>
+							<li><a href="{{$album->album_youtube}}"><i class="fa fa-lg fa-google-plus"></i></a></li>
+						</ul>
+						@endforeach
 					</div>
-					<div class="banner-image">
-						<!--<iframe width="100%" height="166px" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F34019569&amp;auto_play=false&amp;show_artwork=true&amp;color=0066cc"></iframe>-->
-						<iframe width="100%" height="450" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F1696284&amp;auto_play=false&amp;show_artwork=true&amp;color=bfbfbf"></iframe>
-					</div>
-					
-				</div>
-				<div id="songs" class="col-md-3">
-							<ul class="songs">
-								<li>
-									<div class="track-meta">
-										<h5>Nobody Move</h5>
-									</div>
-								</li>
-								<li>
-									<div class="track-meta">
-										<h5>Miss Kitten</h5>
-									</div>
-								</li>
-								<li>
-									<div class="track-meta">
-										<h5>Ignissim Congue</h5>
-									</div>
-								</li>
-								<li>
-									<div class="track-meta">
-										<h5>Risus et Justo</h5>
-									</div>
-								</li>
-								<li>
-									<div class="track-meta">
-										<h5>Mauris</h5>
-									</div>
-								</li>
-								<li>
-									<div class="track-meta">
-										<h5>Pulvinar</h5>
-									</div>
-								</li>
-							</ul>
-						</div>
-				<div class="row album-info">
-					<div class="col-xs-12 col-md-9 col-md-offset-1">
-						<h3>Album Description</h3>
+					<div class="col-sm-8 col-md-9">
+						<h3>Biography Heading of Artist</h3>
 						<p>Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie.</p>
 						<p>Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie.</p>
-						
-						
+						<h4>Smaller heading</h4>
+						<p>Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie.</p>
 					</div>
 				</div>
-
-				
 				</div>
 			</div>
 		</section>
